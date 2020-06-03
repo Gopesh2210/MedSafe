5a84b4f2063a79ef8782c7b5be720604ac4190c9
Gopesh <gopesh2210@gmail.com>
2020-06-03 23:07:46 +0530

    Server update

--- medsafe-server-node/index.js
+++ medsafe-server-node/index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
 const port = 3000
+
+app.use(bodyParser.json())
+app.use(cors())
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(process.env.PORT || port , () => console.log(`Example app listening at http://localhost:${port}`))
